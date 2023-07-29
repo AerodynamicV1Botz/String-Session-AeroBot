ddd4a7478e865fa6bc774b4165fa88bf6cc01c71
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2023-07-29 20:24:43 +0530

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+API_ID = os.getenv("API_ID", "23976642").strip()
+API_HASH = os.getenv("API_HASH", "aa5069357a5fecbf3366beeb73f8c999").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "5363463318:AAHzdOwLzLhnWt8q6Yd0pm2Sn-QRa2If3FY").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+MUST_JOIN = os.getenv("MUST_JOIN", "@AerodynamicV1_Update")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
